1e0061be0bcee7f4355e0315ba53c1a65591d09a
gerardonastri <gerardonastri06@gmail.com>
2024-09-19 19:44:43 +0200

    better

--- src/components/HeroMobile.jsx
+++ src/components/HeroMobile.jsx
@@ -31,13 +31,13 @@ const HeroMobile = () => {
       );
 
   return (
-    <div className='flex md:hidden h-[100vh] flex-col justify-between'>
+    <div className='md:hidden h-[100vh] py-[10px]'>
         <div>
-        <img src={img} className='w-[487px] h-[487px] object-cover' alt='zio sam' /> 
+        <img src={img} className='w-[430px] h-[430px] object-cover' alt='zio sam' /> 
         <h2 className='text-center mt-5 text-6xl text-primary-dark'>{days}G {hours}:{minutes}H</h2>
         <p className='text-primary-dark text-sm text-center'>Il primo passo verso il fondo....</p>
         </div>
-        <TransitionLink href="/home" className='bg-primary-dark hover:bg-[#5f1531] p-5 rounded-3xl w-fit mx-auto flex items-center justify-center mb-[10%]'><FaArrowRight className='text-white text-2xl' /></TransitionLink>
+        <TransitionLink href="/home" className='bg-primary-dark hover:bg-[#5f1531] p-5 rounded-3xl w-fit mx-auto flex items-center justify-center mt-[30px]'><FaArrowRight className='text-white text-2xl' /></TransitionLink>
     </div>
   )
 }
